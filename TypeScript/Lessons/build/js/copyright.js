9fa9e91a0108de74bd8d8e48abc82ded2e9cf1ac
guimendes981 <guilherme.barnabe@grupointegrado.br>
2023-12-19 16:59:02 -0300

    copyright

---
+++ TypeScript/Lessons/build/js/copyright.js
@@ -0,0 +1,11 @@
+"use strict";
+//Original JS code
+const year = document.getElementById('year');
+const thisYear = new Date().getFullYear();
+year.setAttribute('value', thisYear);
+year.textContent = thisYear;
+//Original JS code
+const year = document.getElementById('year');
+const thisYear = new Date().getFullYear();
+year.setAttribute('value', thisYear);
+year.textContent = thisYear;
---
+++ TypeScript/Lessons/src/copyright.ts
@@ -0,0 +1,11 @@
+//Original JS code
+const year = document.getElementById('year');
+const thisYear = new Date().getFullYear();
+year.setAttribute('value', thisYear);
+year.textContent = thisYear
+
+//Original JS code
+const year = document.getElementById('year');
+const thisYear = new Date().getFullYear();
+year.setAttribute('value', thisYear);
+year.textContent = thisYear
