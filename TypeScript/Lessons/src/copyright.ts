c6dbfaf23ef65a2ad9d73bc39b81c4df0dde93ec
guimendes981 <guilherme.barnabe@grupointegrado.br>
2023-12-20 11:00:38 -0300

    Variations of the same TS code

--- TypeScript/Lessons/src/copyright.ts
+++ TypeScript/Lessons/src/copyright.ts
@@ -1,11 +1,24 @@
 //Original JS code
-const year = document.getElementById('year');
-const thisYear = new Date().getFullYear();
-year.setAttribute('value', thisYear);
-year.textContent = thisYear
+// const year = document.getElementById('year');
+// const thisYear = new Date().getFullYear();
+// year.setAttribute('value', thisYear);
+// year.textContent = thisYear
 
-//Original JS code
-const year = document.getElementById('year');
-const thisYear = new Date().getFullYear();
-year.setAttribute('value', thisYear);
-year.textContent = thisYear
+// // First Variation 
+// let year: HTMLElement | null;
+// year = document.getElementById("year");
+// let thisYear: string;
+// thisYear = new Date().getFullYear().toString(); //toString() is converting the number to a string
+// if (year) {
+//   year.setAttribute("dateTime", thisYear);
+//   year.textContent = thisYear;
+// }
+
+//Second Variation of TS Code 
+const year = document.getElementById("year") as HTMLSpanElement; //Sayng that year is a HTMLSpanElement
+let thisYear: string;
+thisYear = new Date().getFullYear().toString(); //toString() is converting the number to a string
+if (year) {
+  year.setAttribute("dateTime", thisYear);
+  year.textContent = thisYear;
+}
